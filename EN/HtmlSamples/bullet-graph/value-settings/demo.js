f5a14cbc2a1fb8e53882c4ca015e51ef02cfbafb
Ignite UI Samples Browser deployer <igniteui.sb@gmail.com>
2013-07-11 18:49:49 +0300

    SB build 13.2.20132.451

--- EN/HtmlSamples/bullet-graph/value-settings/demo.js
+++ EN/HtmlSamples/bullet-graph/value-settings/demo.js
@@ -1,81 +1,164 @@
 $(function () {
-            /*----------------- Instantiation -------------------------*/
+            
             var $bulletGraph = $("#bulletgraph");
 
             $bulletGraph.igBulletGraph({
                 width: "100%",
-                height: "70px",
-                transitionDuration: 1000,
+                height: "80px",
+                transitionDuration: 500,
                 ranges: [
                     {
                         name: 'bad',
                         startValue: 0,
-                        endValue: 50
+                        endValue: 5200
                     },
                     {
                         name: 'acceptable',
-                        startValue: 50,
-                        endValue: 80
+                        startValue: 5200,
+                        endValue: 6400
                     },
                     {
                         name: 'good',
-                        startValue: 80,
-                        endValue: 100
+                        startValue: 6400,
+                        endValue: 10000
                     }
                 ],
                 minimumValue: 0,
-                maximumValue: 100,
+                maximumValue: 10000,
 
-                actualValue: 80,
-                actualValueBrush: 'black',
+                actualValue: 6000,
+                actualValueBrush: 'white',
                 actualValueOutline: 'transparent',
                 actualValueInnerExtent: 0.5,
                 actualValueOuterExtent: 0.65,
 
-                targetValue: 90,
+                targetValue: 5550,
                 targetValueBreadth: 4,
                 targetValueStrokeThickness: 0,
-                targetValueBrush: 'black',
+                targetValueBrush: 'white',
                 targetValueOutline: 'transparent'
             });
+
 
             var $bulletGraph2 = $("#bulletgraph2");
 
             $bulletGraph2.igBulletGraph({
-                width: "600px",
-                height: "120px",
-                transitionDuration: 1000,
+                width: "100%",
+                height: "80px",
+                transitionDuration: 500,
                 ranges: [
                     {
                         name: 'bad',
                         startValue: 0,
-                        endValue: 50
+                        endValue: 3400
                     },
                     {
                         name: 'acceptable',
-                        startValue: 50,
-                        endValue: 80
+                        startValue: 3400,
+                        endValue: 4000
                     },
                     {
                         name: 'good',
-                        startValue: 80,
+                        startValue: 4000,
+                        endValue: 10000
+                    }
+                ],
+                minimumValue: 0,
+                maximumValue: 10000,
+
+                actualValue: 5000,
+                actualValueBrush: 'white',
+                actualValueOutline: 'transparent',
+                actualValueInnerExtent: 0.5,
+                actualValueOuterExtent: 0.65,
+
+                targetValue: 4800,
+                targetValueBreadth: 12,
+                targetValueStrokeThickness: 0,
+                targetValueBrush: 'white',
+                targetValueOutline: 'transparent'
+            });
+
+
+            var $bulletGraph3 = $("#bulletgraph3");
+
+            $bulletGraph3.igBulletGraph({
+                width: "100%",
+                height: "80px",
+                transitionDuration: 500,
+                ranges: [
+                    {
+                        name: 'bad',
+                        startValue: 0,
+                        endValue: 254
+                    },
+                    {
+                        name: 'acceptable',
+                        startValue: 254,
+                        endValue: 300
+                    },
+                    {
+                        name: 'good',
+                        startValue: 300,
+                        endValue: 500
+                    }
+                ],
+                minimumValue: 0,
+                maximumValue: 500,
+
+                actualValue: 350,
+                actualValueBrush: 'white',
+                actualValueOutline: 'transparent',
+                actualValueInnerExtent: 0.35,
+                actualValueOuterExtent: 0.8,
+
+                targetValue: 300,
+                targetValueBreadth: 4,
+                targetValueStrokeThickness: 0,
+                targetValueBrush: 'white',
+                targetValueOutline: 'transparent'
+            });
+
+
+            var $bulletGraph4 = $("#bulletgraph4");
+
+            $bulletGraph4.igBulletGraph({
+                width: "100%",
+                height: "80px",
+                transitionDuration: 500,
+                ranges: [
+                    {
+                        name: 'bad',
+                        startValue: 0,
+                        endValue: 60
+                    },
+                    {
+                        name: 'acceptable',
+                        startValue: 60,
+                        endValue: 70
+                    },
+                    {
+                        name: 'good',
+                        startValue: 70,
                         endValue: 100
                     }
                 ],
                 minimumValue: 0,
                 maximumValue: 100,
 
-                actualValue: 80,
-                actualValueBrush: 'black',
-                actualValueOutline: 'transparent',
-                actualValueInnerExtent: 0.5,
-                actualValueOuterExtent: 0.65,
+                actualValue: 50,
+                actualValueBrush: 'white',
+                actualValueOutline: 'transparent', 
 
-                targetValue: 90,
+                targetValue: 73,
                 targetValueBreadth: 4,
                 targetValueStrokeThickness: 0,
-                targetValueBrush: 'black',
-                targetValueOutline: 'transparent'
+                targetValueBrush: 'white',
+                targetValueOutline: 'transparent',
+
+                formatLabel: function (evt, ui) {
+                    ui.label = ui.value + "%";
+                }
             });
             // Logic for property switches
 
