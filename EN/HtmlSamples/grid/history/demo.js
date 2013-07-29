f6a245cc24aa3fae139a74b5cd5a6e636488efbf
Ignite UI Samples Browser deployer <igniteui.sb@gmail.com>
2013-07-29 19:17:05 +0300

    SB build 13.2.20132.589

---
+++ EN/HtmlSamples/grid/history/demo.js
@@ -0,0 +1,94 @@
+$(function () {
+            // Revert state from browser history
+            window.History.Adapter.bind(window, 'statechange', function () {
+                
+            });
+
+            (function loadInitialStateFromUrl() {
+
+            })();
+
+            function pushToBrowserHistory() {
+                var controlID = args.owner.element[0].id,
+                    widgetName = args.owner.widgetName,
+                    methodSignature = { controlID: controlID, widgetName: widgetName, args: [] };
+                window.History.pushState(methodSignature, e.type, newUrl);
+            }
+
+            $("#grid").igGrid({
+                primaryKey: "EmployeeID",
+                height: '500px',
+                width: '100%',
+                columns: [
+                    { headerText: "Employee ID", key: "EmployeeID", dataType: "number", width: "15%" },
+                    { headerText: "First Name", key: "FirstName", dataType: "string", width: "20%" },
+                    { headerText: "Last Name", key: "LastName", dataType: "string", width: "20%" },
+                    { headerText: "Title", key: "Title", dataType: "string", width: "35%" }
+                ],
+                autofitLastColumn: false,
+                autoGenerateColumns: false,
+                dataSource: northwind,
+                responseDataKey: "results",
+                features: [
+                    {
+                        name: "Paging",
+                        type: "local",
+                        pageSize: 5,
+                        showPageSizeDropDown: false,
+                        pageIndexChanged: function (e, args) {
+                            pushToBrowserHistory(e, args);
+                        }
+                    },
+                    {
+                        name: "Sorting",
+                        type: "local",
+                        columnSorted: function (e, args) {
+
+                        }
+                    },
+                    {
+                        name: "Filtering",
+                        type: "local",
+                        dataFiltered: function (e, args) {
+
+                        }
+                    },
+                    {
+                        name: "Resizing",
+                        columnResized: function (e, args) {
+
+                        }
+                    },
+                    {
+                        name: "Hiding",
+                        columnHidden: function (e, args) {
+
+                        },
+                        columnShown: function (e, args) {
+
+                        }
+                    },
+                    {
+                        name: "GroupBy",
+                        groupedColumnsChanged: function (e, args) {
+
+                        }
+                    },
+                    {
+                        name: "ColumnMoving",
+                        columnMoved: function (e, args) {
+
+                        }
+                    },
+                    {
+                        name: "ColumnFixing",
+                        columnFixed: function (e, args) {
+
+                        },
+                        columnUnfixed: function (e, args) {
+
+                        }
+                    }
+                ]
+            });
+        });
