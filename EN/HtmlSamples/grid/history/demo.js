2edf8eb5a396678d5cc077257388f07c03b349f6
Ignite UI Samples Browser deployer <igniteui.sb@gmail.com>
2013-07-30 19:39:50 +0300

    SB build 13.2.20132.613

--- EN/HtmlSamples/grid/history/demo.js
+++ EN/HtmlSamples/grid/history/demo.js
@@ -8,7 +8,11 @@ $(function () {
 
             })();
 
-            function pushToBrowserHistory() {
+            $("#prev").click(function () { window.History.go(-1); });
+            $("#next").click(function () { window.History.go(1); });
+            $("#copy").click(function () { window.prompt("Use this URL and send it", window.location); });
+
+            function pushToBrowserHistory(e, args) {
                 var controlID = args.owner.element[0].id,
                     widgetName = args.owner.widgetName,
                     methodSignature = { controlID: controlID, widgetName: widgetName, args: [] };
@@ -16,26 +20,31 @@ $(function () {
             }
 
             $("#grid").igGrid({
-                primaryKey: "EmployeeID",
-                height: '500px',
+                primaryKey: "name",
                 width: '100%',
                 columns: [
-                    { headerText: "Employee ID", key: "EmployeeID", dataType: "number", width: "15%" },
-                    { headerText: "First Name", key: "FirstName", dataType: "string", width: "20%" },
-                    { headerText: "Last Name", key: "LastName", dataType: "string", width: "20%" },
-                    { headerText: "Title", key: "Title", dataType: "string", width: "35%" }
+                    { headerText: "Name", key: "name", dataType: "string", width: "12%" },
+                    { headerText: "Team", key: "team", dataType: "string", width: "15%" },
+                    { headerText: "Age", key: "age", dataType: "number", width: "7%" },
+                    { headerText: "No.", key: "number", dataType: "number", width: "7%" },
+                    { headerText: "Pos.", key: "position", dataType: "string", width: "8%" },
+                    { headerText: "Goals", key: "goals", dataType: "number", width: "10%" },
+                    { headerText: "Assists", key: "assists", dataType: "number", width: "10%" },
+                    { headerText: "Yellow C.", key: "yellow", dataType: "number", width: "10%" },
+                    { headerText: "Red C.", key: "red", dataType: "number", width: "7%" },
+                    { headerText: "Salary", key: "salary", format: "currency", width: "8%" }
                 ],
                 autofitLastColumn: false,
                 autoGenerateColumns: false,
-                dataSource: northwind,
-                responseDataKey: "results",
+                dataSource: dataSource,
                 features: [
                     {
                         name: "Paging",
                         type: "local",
-                        pageSize: 5,
+                        pageSize: 10,
                         showPageSizeDropDown: false,
                         pageIndexChanged: function (e, args) {
+                            pageIndex = args.pageIndex;
                             pushToBrowserHistory(e, args);
                         }
                     },
@@ -89,6 +98,11 @@ $(function () {
 
                         }
                     }
-                ]
+                ],
+                rendered: function (e, args) {
+                    args.owner.element.find("tr td").css("text-align", "center");
+                    args.owner.element.find("tr td:first-child").css("text-align", "left");
+                    args.owner.element.find("tr td:last-child").css("text-align", "right");
+                }
             });
         });
