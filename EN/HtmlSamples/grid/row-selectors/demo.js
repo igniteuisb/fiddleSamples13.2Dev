437f783a983acdea293984e70ea1858a2a2b99ec
Ignite UI Samples Browser deployer <igniteui.sb@gmail.com>
2013-08-02 15:26:08 +0300

    SB build 13.2.20132.657

--- EN/HtmlSamples/grid/row-selectors/demo.js
+++ EN/HtmlSamples/grid/row-selectors/demo.js
@@ -1,7 +1,5 @@
-$(function () {           
-
+$(function () {
             /*----------------- Instantiation -------------------------*/
-
             createDefaultSelectorsGrid();
             createCboxSelectorsGrid();
             createRowNumberingGrid();
@@ -9,17 +7,15 @@ $(function () {
 
 
 
-        function createDefaultSelectorsGrid() {            
-
+        function createDefaultSelectorsGrid() {
             $("#defaultRowSelectors").igGrid({
-                height: "360px",
                 width: "98%",
                 autoGenerateColumns: false,
                 dataSource: northwindEmployees,
                 responseDataKey: "results",
                 dataSourceType: "json",
                 columns: [
-                    { headerText: "Employee ID", key: "ID", dataType: "number", width: "100px" },
+                    { headerText: "Employee ID", key: "ID", dataType: "number", width: "120px" },
                     { headerText: "Name", key: "Name", dataType: "string" },
                     { headerText: "Title", key: "Title", dataType: "string"},
                     { headerText: "Phone", key: "Phone", dataType: "string" }
@@ -32,7 +28,7 @@ $(function () {
                             {
                                 columnKey: 'ID',
                                 classes: 'hidden-phone'
-                            }                           
+                            }
                         ]
                     },
                     {
@@ -41,23 +37,21 @@ $(function () {
                         rowSelectorColumnWidth: "50px"
                     },
                     {
-                        name: "Selection",                        
+                        name: "Selection"
                     }
                 ]
             });
         }
 
         function createCboxSelectorsGrid() {
-  
             $("#cbRowSelectors").igGrid({
-                height: "360px",
                 width: "98%",
                 autoGenerateColumns: false,
                 dataSource: northwindEmployees,
                 responseDataKey: "results",
                 dataSourceType: "json",
                 columns: [
-                    { headerText: "Employee ID", key: "ID", dataType: "number", width: "100px" },
+                    { headerText: "Employee ID", key: "ID", dataType: "number", width: "120px" },
                     { headerText: "Name", key: "Name", dataType: "string"},
                     { headerText: "Title", key: "Title", dataType: "string" },
                     { headerText: "Phone", key: "Phone", dataType: "string" }
@@ -80,22 +74,21 @@ $(function () {
                         rowSelectorColumnWidth: "50px"
                     },
                     {
-                        name: "Selection",                        
+                        name: "Selection"
                     }
                 ]
             });
         }
 
         function createRowNumberingGrid() {
-
             $("#rowNumbering").igGrid({
-                height: "360px",
                 width: "98%",
                 autoGenerateColumns: false,
                 dataSource: northwindEmployees,
                 responseDataKey: "results",
                 dataSourceType: "json",
-                columns: [                    
+                columns: [
+                    { headerText: "Employee ID", key: "ID", dataType: "number", width: "120px" },
                     { headerText: "Name", key: "Name", dataType: "string" },
                     { headerText: "Title", key: "Title", dataType: "string" },
                     { headerText: "Phone", key: "Phone", dataType: "string" }
@@ -112,13 +105,13 @@ $(function () {
                         ]
                     },
                     {
-                        name: "RowSelectors",                        
+                        name: "RowSelectors",
                         enableRowNumbering: true,
                         rowSelectorColumnWidth: "50px"
                     },
                     {
                         name: "Selection"
-                    }                    
+                    }
                 ]
             });
         }
--- EN/HtmlSamples/grid/selection/demo.js
+++ EN/HtmlSamples/grid/selection/demo.js
@@ -1,20 +1,17 @@
-$(function () {    
-
+$(function () {
             createCellSelectionGrid();
             createRowSelectionGrid();
         });
 
-        function createCellSelectionGrid() {            
-
+        function createCellSelectionGrid() {
             $("#cellSelectionGrid").igGrid({
-                height: "380px",
                 width: "98%",
                 autoGenerateColumns: false,
                 dataSource: northwindEmployees,
                 responseDataKey: "results",
                 dataSourceType: "json",
                 columns: [
-                    { headerText: "Employee ID", key: "ID", dataType: "number", width: "100px" },
+                    { headerText: "Employee ID", key: "ID", dataType: "number", width: "120px" },
                     { headerText: "Name", key: "Name", dataType: "string" },
                     { headerText: "Title", key: "Title", dataType: "string"},
                     { headerText: "Phone", key: "Phone", dataType: "string" }
@@ -27,7 +24,7 @@ $(function () {
                             {
                                 columnKey: 'ID',
                                 classes: 'hidden-phone'
-                            }                           
+                            }
                         ]
                     },
                     {
@@ -41,17 +38,15 @@ $(function () {
             });
         }
 
-        function createRowSelectionGrid() {           
-  
+        function createRowSelectionGrid() {
             $("#rowSelectionGrid").igGrid({
-                height: "380px",
                 width: "98%",
                 autoGenerateColumns: false,
                 dataSource: northwindEmployees,
                 responseDataKey: "results",
                 dataSourceType: "json",
                 columns: [
-                    { headerText: "Employee ID", key: "ID", dataType: "number", width: "100px" },
+                    { headerText: "Employee ID", key: "ID", dataType: "number", width: "120px" },
                     { headerText: "Name", key: "Name", dataType: "string"},
                     { headerText: "Title", key: "Title", dataType: "string" },
                     { headerText: "Phone", key: "Phone", dataType: "string" }
@@ -66,7 +61,7 @@ $(function () {
                                 classes: 'hidden-phone'
                             }
                         ]
-                    },                    
+                    },
                     {
                         name: "Selection",
                         mode: 'row',
