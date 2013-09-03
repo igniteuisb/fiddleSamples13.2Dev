f69211fce4064d23c8553fc7d9ca584ed970c1e5
Ignite UI Samples Browser deployer <igniteui.sb@gmail.com>
2013-09-03 21:20:32 +0300

    SB build 13.2.20132.952

--- EN/HtmlSamples/data-chart/bind-1-million-data-points/demo.js
+++ EN/HtmlSamples/data-chart/bind-1-million-data-points/demo.js
@@ -44,6 +44,7 @@ var currData, currDataSource, doGeneration, startTime = null;
 	        $("#chart").igDataChart({
 	            width: "100%",
 	            height: "500px",
+	            title: "Random Generated Data",
 	            axes: [{
 	                name: "xAxis",
 	                type: "categoryX",
