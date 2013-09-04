db2043cce58156f92140c45bab76a5fac2a97c83
Ignite UI Samples Browser deployer <igniteui.sb@gmail.com>
2013-09-04 21:34:12 +0300

    SB build 13.2.20132.979

--- EN/HtmlSamples/data-chart/custom-series-highlighting/demo.js
+++ EN/HtmlSamples/data-chart/custom-series-highlighting/demo.js
@@ -1,97 +1,67 @@
 $(function () {
+            
+            var data = [
+                    { "CountryName": "China", "Pop1995": 1216, "Pop2005": 1297, "Pop2015": 1361, "Pop2025": 1394 },
+                    { "CountryName": "India", "Pop1995": 920, "Pop2005": 1090, "Pop2015": 1251, "Pop2025": 1396 },
+                    { "CountryName": "United States", "Pop1995": 266, "Pop2005": 295, "Pop2015": 322, "Pop2025": 351 },
+                    { "CountryName": "Indonesia", "Pop1995": 197, "Pop2005": 229, "Pop2015": 256, "Pop2025": 277 },
+                    { "CountryName": "Brazil", "Pop1995": 161, "Pop2005": 186, "Pop2015": 204, "Pop2025": 218 }
+            ];
+
             $("#chart").igDataChart({
                 width: "100%",
                 height: "400px",
+                title: "Population per Country",
+                subtitle: "Population from 2005",
                 dataSource: data,
-                legend: { element: "lineLegend" },
-                title: "U.K. vs. France",
-                subtitle: "A comparison of populations over time",
-                axes: [{
-                    name: "xAxis",
-                    type: "categoryX",
-                    label: "Date",
-                    isInverted: true,
-                    interval: 2,
-                    title: "Year"
-                },
-                {
-                    name: "yAxis",
-                    type: "numericY",
-                    title: "Population (Millions of People)"
-                }],
+                axes: [
+                    {
+                        name: "NameAxis",
+                        type: "categoryX",
+                        title: "Country",
+                        label: "CountryName"
+                    },
+                    {
+                        name: "PopulationAxis",
+                        type: "numericY",
+                        minimumValue: 0,
+                        title: "Population (Millions of People)",
+                    }
+                ],
+
                 series: [{
-                    name: "ukPop",
-                    type: "column",
-                    title: "UK",
-                    isHighlightingEnabled: true,
-                    isTransitionInEnabled: true,
-                    xAxis: "xAxis",
-                    yAxis: "yAxis",
-                    valueMemberPath: "ukPopulation",
-                    showTooltip: true,
-                    tooltipTemplate: "tooltipTemplate1",
-                    isCustomCategoryStyleAllowed: true,
-                    assigningCategoryStyle: function (e, ui) {
-                        var minOpacity = .3, opacity = 1.0, curr;
-                        alert(ui.sumAllSeriesHighlightingProgress);
-                        if (ui.sumAllSeriesHighlightingProgress > 0.0) {
-                            var progress = 0;
-                            if (ui.highlightingInfo !== null) {
-                                progress = ui.highlightingInfo.progress;
-                            }
-
-                            progress = progress - ui.sumAllSeriesHighlightingProgress;
-
-                            opacity = minOpacity + (1.0 + progress) * (1.0 - minOpacity);
-                            ui.opacity = opacity;
-                            ui.highlightingHandled = true;
-
-                            for (var i = 0; i < series.length; i++) {
-                                curr = series[i];
-                                if (curr.name != ui.series.name) {
-                                    $("#chart").igDataChart("notifyVisualPropertiesChanged", curr.name);
-                                }
-                            }
-                        }
-                    }
-                }, {
-                    name: "frPop",
-                    type: "column",
-                    title: "France",
-                    isTransitionInEnabled: true,
-                    isHighlightingEnabled: true,
-                    xAxis: "xAxis",
-                    yAxis: "yAxis",
-                    valueMemberPath: "francePopulation",
-                    showTooltip: true,
-                    tooltipTemplate: "tooltipTemplate2",
-                    isCustomCategoryStyleAllowed: true,
-                    assigningCategoryStyle: function (e, ui) {
-                        var minOpacity = .3, opacity = 1.0, curr;
-                        alert(ui.sumAllSeriesHighlightingProgress);
-                        if (ui.sumAllSeriesHighlightingProgress > 0.0) {
-                            var progress = 0;
-                            if (ui.highlightingInfo !== null) {
-                                progress = ui.highlightingInfo.progress;
-                            }
-
-                            progress = progress - ui.sumAllSeriesHighlightingProgress;
-
-                            opacity = minOpacity + (1.0 + progress) * (1.0 - minOpacity);
-                            ui.opacity = opacity;
-                            ui.highlightingHandled = true;
-
-                            for (var i = 0; i < series.length; i++) {
-                                curr = series[i];
-                                if (curr.name != ui.series.name) {
-                                    $("#chart").igDataChart("notifyVisualPropertiesChanged", curr.name);
-                                }
-                            }
-                        }
-                    }
+                  name: "2005Population",
+                  type: "column",
+                  title: "2005 Population",  
+                  isHighlightingEnabled: true,
+                  isTransitionInEnabled: true,
+                  xAxis: "NameAxis",
+                  yAxis: "PopulationAxis",
+                  valueMemberPath: "Pop2005",
+                  showTooltip: true,
+                  tooltipTemplate: "tooltipTemplate1",
+                  isCustomCategoryStyleAllowed: true,
+                  isAssigningCategoryStyleAssigned: true
                 }],
                 
                 horizontalZoomable: true,
+                assigningCategoryStyle: function (e, ui) {
+                    var minOpacity = .3, opacity = 1.0, curr;
+                    if (ui.sumAllSeriesHighlightingProgress > 0.0) {
+                        var progress = 0;
+                        if (ui.highlightingInfo !== null) {
+                            progress = ui.highlightingInfo.progress;
+                        }
+
+                        progress = progress - ui.sumAllSeriesHighlightingProgress;
+
+                        opacity = minOpacity + (1.0 + progress) * (1.0 - minOpacity);
+                        ui.opacity = opacity;
+                        ui.highlightingHandled = true;
+
+                        
+                    }
+                },
                 verticalZoomable: true,
                 windowResponse: "immediate"
             });
