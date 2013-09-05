c1ee6160104c788c0c7c9aca002e5d10d974e328
Ignite UI Samples Browser deployer <igniteui.sb@gmail.com>
2013-09-05 23:51:59 +0300

    SB build 13.2.20132.1003

---
+++ JA/HtmlSamples/data-chart/transition-animation/demo.js
@@ -0,0 +1,167 @@
+$(function () {
+            $("#chart").igDataChart({
+                width: "100%",
+                height: "400px",
+                dataSource: data,
+                legend: { element: "lineLegend" },
+                title: "U.K. vs. フランス",
+                subtitle: "人口の比較 (年)",
+                axes: [{
+                    name: "xAxis",
+                    type: "categoryX",
+                    label: "Date",
+                    isInverted: true,
+                    interval: 2,
+                    title: "年"
+                },
+                {
+                    name: "yAxis",
+                    type: "numericY",
+                    title: "人口 (百万人単位)"
+                }],
+                series: [{
+                    name: "ukPop",
+                    type: "line",
+                    title: "UK",
+                    isHighlightingEnabled: true,
+                    isTransitionInEnabled: true,
+                    xAxis: "xAxis",
+                    yAxis: "yAxis",
+                    valueMemberPath: "ukPopulation",
+                    showTooltip: true,
+                    tooltipTemplate: "tooltipTemplate1",
+                    thickness: 5
+                }, {
+                    name: "frPop",
+                    type: "line",
+                    title: "France",
+                    isTransitionInEnabled: true,
+                    isHighlightingEnabled: true,
+                    xAxis: "xAxis",
+                    yAxis: "yAxis",
+                    valueMemberPath: "francePopulation",
+                    showTooltip: true,
+                    tooltipTemplate: "tooltipTemplate2",
+                    thickness: 5
+                }],
+                horizontalZoomable: true,
+                verticalZoomable: true,
+                windowResponse: "immediate"
+            });
+
+            $("#seriesType").change(function (e) {
+                transMode = $("#transitionInTypeSlider").val();
+                transType = $("#transitionInSpeedTypeSlider").val();
+                transEasingFunc = $("#transitionEasingFunctionSlider").val();
+                
+                var thickness = 5,
+                    seriesType = $(this).val();
+
+                if (seriesType == "area" ||
+                    seriesType == "splineArea" ||
+                    seriesType == "column" ||
+                    seriesType == "stepArea") {
+                    thickness = 1;
+                }
+
+                $("#chart").igDataChart("option", "series", [{ name: "ukPop", remove: true }]);
+                $("#chart").igDataChart("option", "series", [{ name: "frPop", remove: true }]);
+                $("#chart").igDataChart("option", "series", [{
+                    name: "ukPop",
+                    title: "UK",
+                    type: $(this).val(),
+                    isTransitionInEnabled: true,
+                    xAxis: "xAxis",
+                    yAxis: "yAxis",
+                    valueMemberPath: "ukPopulation",
+                    isHighlightingEnabled: true,
+                    showTooltip: true,
+                    tooltipTemplate: "tooltipTemplate1",
+                    thickness: thickness,
+                    transitionInMode: transMode,
+                    transitionInSpeedType: transType,
+                    transitionEasingFunction: transEasingFunc
+                }]);
+                $("#chart").igDataChart("option", "series", [{
+                    name: "frPop",
+                    title: "France",
+                    type: $(this).val(),
+                    xAxis: "xAxis",
+                    yAxis: "yAxis",
+                    valueMemberPath: "francePopulation",
+                    isHighlightingEnabled: true,
+                    isTransitionInEnabled: true,
+                    showTooltip: true,
+                    tooltipTemplate: "tooltipTemplate2",
+                    thickness: thickness,
+                    transitionInMode: transMode,
+                    transitionInSpeedType: transType,
+                    transitionEasingFunction: transEasingFunc
+                }]);
+            });
+
+            var redrawChart = function () {
+                
+                transMode = $("#transitionInTypeSlider").val();
+                transType = $("#transitionInSpeedTypeSlider").val();
+                transEasingFunc = $("#transitionEasingFunctionSlider").val();
+
+                var thickness = 5,
+                    seriesType = $("#seriesType").val();
+
+                if (seriesType == "area" ||
+                    seriesType == "splineArea" ||
+                    seriesType == "column" ||
+                    seriesType == "stepArea") {
+                    thickness = 1;
+                }
+                
+                $("#chart").igDataChart("option", "series", [{ name: "ukPop", remove: true }]);
+                $("#chart").igDataChart("option", "series", [{ name: "frPop", remove: true }]);
+                $("#chart").igDataChart("option", "series", [{
+                    name: "ukPop",
+                    title: "UK",
+                    type: $("#seriesType").val(),
+                    isTransitionInEnabled: true,
+                    xAxis: "xAxis",
+                    yAxis: "yAxis",
+                    valueMemberPath: "ukPopulation",
+                    isHighlightingEnabled: true,
+                    showTooltip: true,
+                    tooltipTemplate: "tooltipTemplate1",
+                    thickness: thickness,
+                    transitionInMode: transMode,
+                    transitionInSpeedType: transType,
+                    transitionEasingFunction: transEasingFunc
+                }]);
+                $("#chart").igDataChart("option", "series", [{
+                    name: "frPop",
+                    title: "France",
+                    type: $("#seriesType").val(),
+                    xAxis: "xAxis",
+                    yAxis: "yAxis",
+                    valueMemberPath: "francePopulation",
+                    isHighlightingEnabled: true,
+                    isTransitionInEnabled: true,
+                    showTooltip: true,
+                    tooltipTemplate: "tooltipTemplate2",
+                    thickness: thickness,
+                    transitionInMode: transMode,
+                    transitionInSpeedType: transType,
+                    transitionEasingFunction: transEasingFunc
+                }]);
+
+            };
+           
+            $("#transitionInTypeSlider").change(function (e) {
+                redrawChart();
+            });
+
+            $("#transitionInSpeedTypeSlider").change(function (e) {
+                redrawChart();
+            });
+
+            $("#transitionEasingFunctionSlider").change(function (e) {
+                redrawChart();
+            });
+        });
