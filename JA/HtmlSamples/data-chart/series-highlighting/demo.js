0bfcde9fc618c575d6397d0066b36d29c8e93b28
Ignite UI Samples Browser deployer <igniteui.sb@gmail.com>
2013-09-10 00:10:08 +0300

    SB build 13.2.20132.1032

--- JA/HtmlSamples/data-chart/series-highlighting/demo.js
+++ JA/HtmlSamples/data-chart/series-highlighting/demo.js
@@ -1,50 +1,57 @@
 $(function () {
+
+            var data = [
+                { "CountryName": "China", "Pop1995": 1216, "Pop2005": 1297, "Pop2015": 1361, "Pop2025": 1394 },
+                { "CountryName": "India", "Pop1995": 920, "Pop2005": 1090, "Pop2015": 1251, "Pop2025": 1396 },
+                { "CountryName": "United States", "Pop1995": 266, "Pop2005": 295, "Pop2015": 322, "Pop2025": 351 },
+                { "CountryName": "Indonesia", "Pop1995": 197, "Pop2005": 229, "Pop2015": 256, "Pop2025": 277 },
+                { "CountryName": "Brazil", "Pop1995": 161, "Pop2005": 186, "Pop2015": 204, "Pop2025": 218 }
+            ];
+
             $("#chart").igDataChart({
                 width: "100%",
                 height: "400px",
-                dataSource: data,
                 legend: { element: "lineLegend" },
-                title: "U.K. vs. フランス",
-                subtitle: "人口の比較 (年)",
-                axes: [{
-                    name: "xAxis",
-                    type: "categoryX",
-                    label: "Date",
-                    isInverted: true,
-                    interval: 2,
-                    title: "年"
-                },
-                {
-                    name: "yAxis",
-                    type: "numericY",
-                    title: "人口 (百万人単位)"
-                }],
-                series: [{
-                    name: "ukPop",
-                    type: "line",
-                    title: "UK",
-                    isHighlightingEnabled: true,
-                    isTransitionInEnabled: true,
-                    xAxis: "xAxis",
-                    yAxis: "yAxis",
-                    valueMemberPath: "ukPopulation",
-                    showTooltip: true,
-                    thickness: 5
-                }, {
-                    name: "frPop",
-                    type: "line",
-                    title: "France",
-                    isTransitionInEnabled: true,
-                    isHighlightingEnabled: true,
-                    xAxis: "xAxis",
-                    yAxis: "yAxis",
-                    valueMemberPath: "francePopulation",
-                    showTooltip: true,
-                    thickness: 5
-                }],
-                horizontalZoomable: true,
-                verticalZoomable: true,
-                windowResponse: "immediate"
+                title: "国別人口",
+                subtitle: "1995 年と 2005 年の人口の比較",
+                dataSource: data,
+                axes: [
+                    {
+                        name: "NameAxis",
+                        type: "categoryX",
+                        label: "CountryName"
+                    },
+                    {
+                        name: "PopulationAxis",
+                        type: "numericY",
+                        minimumValue: 0,
+                        title: "人口 (百万人単位)",
+                    }
+                ],
+                series: [
+                    {
+                        name: "2005Population",
+                        type: "line",
+                        title: "2005",
+                        xAxis: "NameAxis",
+                        yAxis: "PopulationAxis",
+                        valueMemberPath: "Pop2005",
+                        isTransitionInEnabled: true,
+                        isHighlightingEnabled: true,
+                        thickness: 5
+                    },
+                    {
+                        name: "1995Population",
+                        type: "line",
+                        title: "1995",
+                        xAxis: "NameAxis",
+                        yAxis: "PopulationAxis",
+                        valueMemberPath: "Pop1995",
+                        isTransitionInEnabled: true,
+                        isHighlightingEnabled: true,
+                        thickness: 5
+                    }
+                ]
             });
 
             $("#seriesType").change(function (e) {
@@ -54,37 +61,37 @@ $(function () {
                 if (seriesType == "area" ||
                     seriesType == "splineArea" ||
                     seriesType == "column" ||
+                    seriesType == "waterfall" ||
                     seriesType == "stepArea") {
                     thickness = 1;
                 }
 
-                $("#chart").igDataChart("option", "series", [{ name: "ukPop", remove: true }]);
-                $("#chart").igDataChart("option", "series", [{ name: "frPop", remove: true }]);
+                $("#chart").igDataChart("option", "series", [{ name: "2005Population", remove: true }]);
+                $("#chart").igDataChart("option", "series", [{ name: "1995Population", remove: true }]);
                 $("#chart").igDataChart("option", "series", [{
-                    name: "ukPop",
-                    title: "UK",
                     type: $(this).val(),
+                    name: "2005Population",
+                    title: "2005",
+                    xAxis: "NameAxis",
+                    yAxis: "PopulationAxis",
+                    valueMemberPath: "Pop2005",
                     isTransitionInEnabled: true,
-                    xAxis: "xAxis",
-                    yAxis: "yAxis",
-                    valueMemberPath: "ukPopulation",
                     isHighlightingEnabled: true,
-                    showTooltip: true,
                     thickness: thickness
                 }]);
                 $("#chart").igDataChart("option", "series", [{
-                    name: "frPop",
-                    title: "France",
                     type: $(this).val(),
-                    xAxis: "xAxis",
-                    yAxis: "yAxis",
-                    valueMemberPath: "francePopulation",
-                    isHighlightingEnabled: true,
+                    name: "1995Population",
+                    title: "1995",
+                    xAxis: "NameAxis",
+                    yAxis: "PopulationAxis",
+                    valueMemberPath: "Pop1995",
                     isTransitionInEnabled: true,
-                    showTooltip: true,
+                    isHighlightingEnabled: true,
                     thickness: thickness
                 }]);
             });
+      
             
             // Transiton Duration Slider
             $("#transitionDurationSlider").slider({
