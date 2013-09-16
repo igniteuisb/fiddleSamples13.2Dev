e5656478839fa755545414897f793409bd24bc81
Ignite UI Samples Browser deployer <igniteui.sb@gmail.com>
2013-09-16 21:32:08 +0300

    SB build 13.2.20132.1135

--- JA/HtmlSamples/grid/json-binding/demo.js
+++ JA/HtmlSamples/grid/json-binding/demo.js
@@ -7,7 +7,7 @@ $(function () {
                 ];
             
             // temporary use id="grid" instead of id="grid7": wait/undo if igGrid will fix its bug
-				$("#grid").igGrid({				
-				    dataSource: data //JSON Array defined above                     
+            $("#grid").igGrid({
+                dataSource: data //JSON Array defined above                     
             });
         });
