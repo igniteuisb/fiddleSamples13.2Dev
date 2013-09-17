97823ffa0d19d9256d5ef76f4836515618170632
Ignite UI Samples Browser deployer <igniteui.sb@gmail.com>
2013-09-18 00:00:09 +0300

    SB build 13.2.20132.1150

--- EN/HtmlSamples/data-chart/category-tooltip-layer/demo.js
+++ EN/HtmlSamples/data-chart/category-tooltip-layer/demo.js
@@ -36,7 +36,9 @@ $(function () {
                         isTransitionInEnabled: true,
                         xAxis: "NameAxis",
                         yAxis: "PopulationAxis",
-                        valueMemberPath: "Pop2005"
+                        valueMemberPath: "Pop2005",
+                        showTooltip: true,
+                        tooltipTemplate: "tooltipTemplate1"
                     },
                     {
                         name: "1995Population",
@@ -45,61 +47,61 @@ $(function () {
                         isTransitionInEnabled: true,
                         xAxis: "NameAxis",
                         yAxis: "PopulationAxis",
-                        valueMemberPath: "Pop1995"
+                        valueMemberPath: "Pop1995",
+                        showTooltip: true,
+                        tooltipTemplate: "tooltipTemplate2"
                     },
                     {
-                        name: "crosshairLayer",
-                        title: "crosshair",
-                        type: "crosshairLayer",
-                        useInterpolation: false,
-                        transitionDuration: 500
+                        name: "categorySeries",
+                        type: "categoryToolTipLayer",
+                        useInterpolation: false
                     }]
             });
 
-            // Brush
-            $("#brush").on({
-                change: function (e) {
-                    var brushColor = $(this).val();
-                    $("#chart").igDataChart("option", "series", [{ name: "crosshairLayer", brush: brushColor }]);
-                }
-            });
+            //// Brush
+            //$("#brush").on({
+            //    change: function (e) {
+            //        var brushColor = $(this).val();
+            //        $("#chart").igDataChart("option", "series", [{ name: "crosshairLayer", brush: brushColor }]);
+            //    }
+            //});
 
-            // Thickness 
-            $("#thicknessSlider").slider({
-                min: 0,
-                max: 10,
-                value: 2,
-                slide: function (event, ui) {
-                    $("#chart").igDataChart("option", "series", [{ name: "crosshairLayer", thickness: ui.value }]);
-                    $("#thicknessLabel").text(ui.value);
-                }
-            });
+            //// Thickness 
+            //$("#thicknessSlider").slider({
+            //    min: 0,
+            //    max: 10,
+            //    value: 2,
+            //    slide: function (event, ui) {
+            //        $("#chart").igDataChart("option", "series", [{ name: "crosshairLayer", thickness: ui.value }]);
+            //        $("#thicknessLabel").text(ui.value);
+            //    }
+            //});
 
-            // Opacity
-            $("#opacitySlider").slider({
-                min: 0,
-                max: 100,
-                value: 50,
-                slide: function (event, ui) {
-                    $("#chart").igDataChart("option", "series", [{ name: "crosshairLayer", opacity: ui.value }]);
-                    $("#opacityLabel").text(ui.value);
-                }
-            });
+            //// Opacity
+            //$("#opacitySlider").slider({
+            //    min: 0,
+            //    max: 100,
+            //    value: 50,
+            //    slide: function (event, ui) {
+            //        $("#chart").igDataChart("option", "series", [{ name: "crosshairLayer", opacity: ui.value }]);
+            //        $("#opacityLabel").text(ui.value);
+            //    }
+            //});
 
-            // Transiton Duration Slider
-            $("#transitionDurationSlider").slider({
-                min: 0,
-                max: 1000,
-                value: 500,
-                slide: function (event, ui) {
-                    $("#chart").igDataChart("option", "series", [{ name: "crosshairLayer", transitionDuration: ui.value }]);
-                    $("#transitionDurationLabel").text(ui.value);
-                }
-            });
+            //// Transiton Duration Slider
+            //$("#transitionDurationSlider").slider({
+            //    min: 0,
+            //    max: 1000,
+            //    value: 500,
+            //    slide: function (event, ui) {
+            //        $("#chart").igDataChart("option", "series", [{ name: "crosshairLayer", transitionDuration: ui.value }]);
+            //        $("#transitionDurationLabel").text(ui.value);
+            //    }
+            //});
 
-            // Use Interpolation
-            $("#useInterpolationCheckBox").click(function (e) {
-                var useInterpolationResult = $("#useInterpolationCheckBox").is(":checked") ? true : false;
-                $("#chart").igDataChart("option", "series", [{ name: "crosshairLayer", useInterpolation: useInterpolationResult }]);
-            });
+            //// Use Interpolation
+            //$("#useInterpolationCheckBox").click(function (e) {
+            //    var useInterpolationResult = $("#useInterpolationCheckBox").is(":checked") ? true : false;
+            //    $("#chart").igDataChart("option", "series", [{ name: "crosshairLayer", useInterpolation: useInterpolationResult }]);
+            //});
         });
