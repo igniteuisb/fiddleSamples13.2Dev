587cac1db16012046e0d7929a3ffc0d551a8ea7d
Ignite UI Samples Browser deployer <igniteui.sb@gmail.com>
2013-09-18 17:29:46 +0300

    SB build 13.2.20132.1158

--- EN/HtmlSamples/data-chart/category-tooltip-layer/demo.js
+++ EN/HtmlSamples/data-chart/category-tooltip-layer/demo.js
@@ -32,24 +32,18 @@ $(function () {
                     {
                         name: "2005Population",
                         type: "column",
-                        isHighlightingEnabled: true,
-                        isTransitionInEnabled: true,
                         xAxis: "NameAxis",
                         yAxis: "PopulationAxis",
                         valueMemberPath: "Pop2005",
-                        showTooltip: true,
-                        tooltipTemplate: "tooltipTemplate1"
+                        showTooltip: true
                     },
                     {
                         name: "1995Population",
-                        type: "line",
-                        isHighlightingEnabled: true,
-                        isTransitionInEnabled: true,
+                        type: "column",
                         xAxis: "NameAxis",
                         yAxis: "PopulationAxis",
                         valueMemberPath: "Pop1995",
-                        showTooltip: true,
-                        tooltipTemplate: "tooltipTemplate2"
+                        showTooltip: true
                     },
                     {
                         name: "categorySeries",
@@ -58,50 +52,28 @@ $(function () {
                     }]
             });
 
-            //// Brush
-            //$("#brush").on({
-            //    change: function (e) {
-            //        var brushColor = $(this).val();
-            //        $("#chart").igDataChart("option", "series", [{ name: "crosshairLayer", brush: brushColor }]);
-            //    }
-            //});
+            //Transiton Duration Slider
+            $("#transitionDurationSlider").slider({
+                min: 0,
+                max: 1000,
+                value: 500,
+                slide: function (event, ui) {
+                    $("#chart").igDataChart("option", "series", [{ name: "categorySeries", transitionDuration: ui.value }]);
+                    $("#transitionDurationLabel").text(ui.value);
+                }
+            });
 
-            //// Thickness 
-            //$("#thicknessSlider").slider({
-            //    min: 0,
-            //    max: 10,
-            //    value: 2,
-            //    slide: function (event, ui) {
-            //        $("#chart").igDataChart("option", "series", [{ name: "crosshairLayer", thickness: ui.value }]);
-            //        $("#thicknessLabel").text(ui.value);
-            //    }
-            //});
-
-            //// Opacity
-            //$("#opacitySlider").slider({
-            //    min: 0,
-            //    max: 100,
-            //    value: 50,
-            //    slide: function (event, ui) {
-            //        $("#chart").igDataChart("option", "series", [{ name: "crosshairLayer", opacity: ui.value }]);
-            //        $("#opacityLabel").text(ui.value);
-            //    }
-            //});
-
-            //// Transiton Duration Slider
-            //$("#transitionDurationSlider").slider({
-            //    min: 0,
-            //    max: 1000,
-            //    value: 500,
-            //    slide: function (event, ui) {
-            //        $("#chart").igDataChart("option", "series", [{ name: "crosshairLayer", transitionDuration: ui.value }]);
-            //        $("#transitionDurationLabel").text(ui.value);
-            //    }
-            //});
-
-            //// Use Interpolation
-            //$("#useInterpolationCheckBox").click(function (e) {
-            //    var useInterpolationResult = $("#useInterpolationCheckBox").is(":checked") ? true : false;
-            //    $("#chart").igDataChart("option", "series", [{ name: "crosshairLayer", useInterpolation: useInterpolationResult }]);
-            //});
+            //Use Interpolation
+            $("#useInterpolationCheckBox").click(function (e) {
+                var useInterpolationResult = $("#useInterpolationCheckBox").is(":checked") ? true : false;
+                $("#chart").igDataChart("option", "series", [{ name: "categorySeries", useInterpolation: useInterpolationResult }]);
+            });
+            
+            $("#tooltipPos").change(function (e) {
+               var position = $(this).val();
+                alert(position);
+               $("#chart").igDataChart("option", "series", [{ name: "categorySeries", tooltipPosition: position }]);
+                
+            });
+            
         });
