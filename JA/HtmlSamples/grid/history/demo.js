61bd427960a4e2d8d27561e287e92618c2b2fa86
Ignite UI Samples Browser deployer <igniteui.sb@gmail.com>
2013-10-01 11:21:35 -0400

    SB build 13.2.20132.1321

--- JA/HtmlSamples/grid/history/demo.js
+++ JA/HtmlSamples/grid/history/demo.js
@@ -1,174 +1,220 @@
 $(function () {
-            var grid = $("#grid"),
-                manualStateChange = false, // true: fired by history go() and back() methods; false: fired when state is added to the history object.
-            	tempParams = "";
+			var grid = $("#grid"),
+				manualStateChange = false, // true: fired by history go() and back() methods; false: fired when state is added to the history object.
+				reverseState = [],
+				stateCount = window.History.storedStates.length;
+				tempParams = "";
+
 
 			//--> Save igGrid state in the browser history object
-            function pushToBrowserHistory(state, title, url) {
-            	manualStateChange = false;
-            	$(state).extend({ "manualStateChange": false });
-            	window.History ? window.History.pushState(state, title, url) : "";
-            }
+			function pushToBrowserHistory(state, title, url) {
+				manualStateChange = false;
+				$(state).extend({ "manualStateChange": false });
+				window.History ? window.History.pushState(state, title, url) : "";
+			}
 
-            grid.igGrid({
-            	primaryKey: "name",
-            	width: '100%',
-            	columns: [
-                    { headerText: "名前", key: "name", dataType: "string", width: "12%" },
-                    { headerText: "チーム", key: "team", dataType: "string", width: "15%" },
-                    { headerText: "年齢", key: "age", dataType: "number", width: "7%" },
-                    { headerText: "番号", key: "number", dataType: "number", width: "7%" },
-                    { headerText: "ポジション", key: "position", dataType: "string", width: "8%" },
-                    { headerText: "ゴール", key: "goals", dataType: "number", width: "10%" },
-                    { headerText: "アシスト", key: "assists", dataType: "number", width: "10%" },
-                    { headerText: "イエローカード", key: "yellow", dataType: "number", width: "10%" },
-                    { headerText: "レッドカード", key: "red", dataType: "number", width: "7%" },
-                    { headerText: "給与", key: "salary", format: "currency", width: "8%" }
-            	],
-            	autofitLastColumn: false,
-            	autoGenerateColumns: false,
-            	dataSource: dataSource,
-            	features: [
-                    {
-                    	name: "Paging",
-                    	type: "local",
-                    	pageSize: 10,
-                    	showPageSizeDropDown: false,
-                    	pageIndexChanged: function (e, args) {
-                    		var pageIndex = args.pageIndex + 1,
-                                state = { key: "page", value: pageIndex },
-                                title = "Paging";
-                    		pushToBrowserHistory(state, title, formURL("page", pageIndex));
-                    	}
-                    },
-                    {
-                    	name: "Sorting",
-                    	type: "local",
-                    	mode: "multi",
-                    	columnSorted: function (e, args) {
-                    		var columnKey = args.columnKey,
-                                dir = args.direction,
-                                state = { key: "sort", value: [columnKey, dir] },
-                                title = "Sorting";
-                    		pushToBrowserHistory(state, title, formURL("sort", [columnKey, dir]));
-                    	}
-                    },
-                    {
-                    	name: "Filtering",
-                    	type: "local",
-                    	dataFiltered: function (e, args) {
-                    		var columnKey = args.columnKey,
-                                expr = args.owner.grid.dataSource.settings.filtering.expressions[0],
-                                title = "Filtering",
-                                state, settings;
-                    		if (expr === undefined) {
-                    			settings = [];
-                    		} else {
-                    			settings = [columnKey, expr.cond, expr.expr];
-                    		}
-                    		state = { key: "filter", value: settings };
-                    		pushToBrowserHistory(state, title, formURL("filter", settings));
-                    	}
-                    },
-                    {
-                    	name: "Resizing",
-                    	columnResized: function (e, args) {
-                    		var columnKey = args.columnKey,
-                               width = args.originalWidth,
-                               state = { key: "resize", value: [columnKey, width] },
-                               title = "Resizing";
-                    		pushToBrowserHistory(state, title, formURL("resize", [columnKey, width]));
-                    	}
-                    },
-                    {
-                    	name: "Hiding",
-                    	columnHidden: function (e, args) {
-                    		var columnKey = args.columnKey,
-                               state = { key: "hide", value: columnKey },
-                               title = "Hiding";
-                    		pushToBrowserHistory(state, title, formURL("hide", [columnKey, true]));
-                    	},
-                    	columnShown: function (e, args) {
-                    		var columnKey = args.columnKey,
-                               state = { key: "hide", value: columnKey },
-                               title = "Hiding";
-                    		pushToBrowserHistory(state, title, formURL("hide", [columnKey, false]));
-                    	}
-                    },
-                    {
-                    	name: "GroupBy",
-                    	groupedColumnsChanged: function (e, args) {
-                    		var columnKey = args.key;
-                    		state = { key: "group", value: columnKey },
-							title = "GroupBy";
-                    		pushToBrowserHistory(state, title, formURL("group", columnKey));
-                    	}
-                    }
-            	],
-            	rendered: function (e, args) {
-            		args.owner.element.find("tr td").css("text-align", "center");
-            		args.owner.element.find("tr td:first-child").css("text-align", "left");
-            		args.owner.element.find("tr td:last-child").css("text-align", "right");
-            	}
-            });
-        	//<-- Save igGrid state in the browser history object
+			(function initGrid() {
+				grid.igGrid({
+					primaryKey: "name",
+					width: '100%',
+					columns: [
+						{ headerText: "名前", key: "name", dataType: "string", width: "12%" },
+						{ headerText: "チーム", key: "team", dataType: "string", width: "15%" },
+						{ headerText: "年齢", key: "age", dataType: "number", width: "7%" },
+						{ headerText: "番号", key: "number", dataType: "number", width: "7%" },
+						{ headerText: "ポジション", key: "position", dataType: "string", width: "8%" },
+						{ headerText: "ゴール", key: "goals", dataType: "number", width: "10%" },
+						{ headerText: "アシスト", key: "assists", dataType: "number", width: "10%" },
+						{ headerText: "イエローカード", key: "yellow", dataType: "number", width: "10%" },
+						{ headerText: "レッドカード", key: "red", dataType: "number", width: "7%" },
+						{ headerText: "給与", key: "salary", format: "currency", width: "8%" }
+					],
+					autofitLastColumn: false,
+					autoGenerateColumns: false,
+					dataSource: dataSource,
+					features: [
+						{
+							name: "Paging",
+							type: "local",
+							pageSize: 10,
+							showPageSizeDropDown: false,
+							pageIndexChanging: function (e, args) {
+								debugger;
+								fillReverseState("page", null, args.currentPageIndex);
+							},
+							pageIndexChanged: function (e, args) {
+								var pageIndex = args.pageIndex + 1,
+									state = { key: "page", value: pageIndex };
+								pushToBrowserHistory(state, null, formURL("page", pageIndex));
+							}
+						},
+						{
+							name: "Sorting",
+							type: "local",
+							mode: "multi",
+							columnSorting: function (e, args) {
+								debugger;
+								fillReverseState("sort", args.columnKey, args.direction === "ascending" ? "descending" : "ascending");
+							},
+							columnSorted: function (e, args) {
+								var columnKey = args.columnKey,
+									dir = args.direction,
+									state = { key: "sort", value: [columnKey, dir] };
+								if (!isEmptyValue(columnKey) && !isEmptyValue(dir)) {
+									pushToBrowserHistory(state, null, formURL("sort", [columnKey, dir]));
+								}
+							}
+						},
+						{
+							name: "Filtering",
+							type: "local",
+							dataFiltered: function (e, args) {
+								var columnKey = args.columnKey,
+									expr = args.owner.grid.dataSource.settings.filtering.expressions[0],
+									state, settings;
+								if (expr === undefined) {
+									settings = [];
+								} else {
+									settings = [columnKey, expr.cond, expr.expr];
+								}
+								state = { key: "filter", value: settings };
+								pushToBrowserHistory(state, null, formURL("filter", settings));
+							}
+						},
+						{
+							name: "Resizing",
+							columnResized: function (e, args) {
+								var columnKey = args.columnKey,
+								   width = args.originalWidth,
+								   state = { key: "resize", value: [columnKey, width] };
+								pushToBrowserHistory(state, null, formURL("resize", [columnKey, width]));
+							}
+						},
+						{
+							name: "Hiding",
+							columnHidden: function (e, args) {
+								var columnKey = args.columnKey,
+								   state = { key: "hide", value: columnKey };
+								pushToBrowserHistory(state, null, formURL("hide", [columnKey, true]));
+							},
+							columnShown: function (e, args) {
+								var columnKey = args.columnKey,
+								   state = { key: "hide", value: columnKey };
+								pushToBrowserHistory(state, null, formURL("hide", [columnKey, false]));
+							}
+						},
+						{
+							name: "GroupBy",
+							groupedColumnsChanged: function (e, args) {
+								var columnKey = args.key;
+								state = { key: "group", value: columnKey };
+								pushToBrowserHistory(state, null, formURL("group", columnKey));
+							}
+						}
+					],
+					rendered: function (e, args) {
+						args.owner.element.find("tr td").css("text-align", "center");
+						args.owner.element.find("tr td:first-child").css("text-align", "left");
+						args.owner.element.find("tr td:last-child").css("text-align", "right");
+					}
+				});
+			})();
 
-        	//--> Recover igGrid state from the browser history object
-            if (window.History && window.History.Adapter) {
+			function fillReverseState(feature, column, oldValue) {
+				var index = window.History.savedStates.length, pos;
+				if (reverseState.length === 0) for (index = 0; index < window.History.savedStates.length; index++) reverseState[index] = null;
+				pos = previousPosition(feature, column);
+				if (pos < 0) {
+					reverseState[index] = oldValue;
+				} else if (pos === 0) {
+					reverseState[index] = null;
+				} else {
+					reverseState[index] = pos.data.value;
+				}
+			}
+
+			function previousPosition(feature, column) {
+				var states = window.History.savedStates,
+					length = states.length,
+					index;
+				for (index = length - 1; index >= 0; index--) {
+					if (states[index].data.key === feature &&
+						(column === null || column !== null && states[index].value.indexOf(column) > -1)) {
+						if (index = length - 1) {
+							return 0;
+						} else {
+							return states[index];
+						}
+					} else {
+						return -1;
+					}
+				}
+			}
+			//<-- Save igGrid state in the browser history object
+
+			//--> Recover igGrid state from the browser history object
+			if (window.History && window.History.Adapter) {
 				window.History.Adapter.bind(window, 'statechange', function (e, args) {
-					var currState, state, stateOccurances;
+					var currState, state, prevState, stateOccurances;
 					if (manualStateChange == true) { // Fired only when called externally from browser buttons
 						debugger;
 						currState = window.History.getState()
 						state = currState.data,
-						stateOccurances = getPreviousStateOccurances(currState.id);
-						if (state.key !== undefined) { // If there is at least one gird state in the browser history
-							if (stateOccurances % 2 === 0) {
-								switch (state.key) { // Load current state
-									case "page": grid.igGridPaging("pageIndex", state.value - 1); break;
-									case "sort": grid.igGridSorting("sortColumn", state.value[0], state.value[1]); break;
-									case "resize": grid.igGridResizing("resize", state.value[0], state.value[1]); break;
-									case "group":
-										grid.igGridGroupBy("ungroupAll");
-										if (!grid.igGridGroupBy("checkColumnIsGrouped")) grid.igGridGroupBy("groupByColumn", state.value);
-										break;
-									case "hide":
-										if (state.value.split("_")[1]) {
-											grid.igGridHiding("hideColumn", state[0]);
-										} else {
-											grid.igGridHiding("showColumn", state[0]);
-										}
-										break;
-									case "filter": grid.igGridFiltering("filter", ([{ fieldName: state.value[0], expr: state.value[2], cond: state.value[1] }])); break;
-								}
-							} else { // Unload previous state
-								switch (state.key) { 
-									case "filter": grid.igGridFiltering("filter", ([])); break;
-									case "sort": grid.igGridSorting("unsortColumn", state.value[0], state.value[1]); break;
-									default: break;
-                        		}
-							}
-						} else {
-                    		// set default igGrid state
-                    		grid.igGridPaging("pageIndex", 0);
+						isBack,
+						lastOccurence = getLastOccurance(state),
+						stateOccurances = getStateOccurances(currState.id);
 
+						isBack = (window.History.storedStates.length - stateCount) === 1;
+						stateCount = window.History.storedStates.length;
+						if (!isBack) {
+							switch (state.key) { // Load current state
+								case "page": grid.igGridPaging("pageIndex", state.value - 1); break;
+								case "sort": grid.igGridSorting("sortColumn", state.value[0], state.value[1]); break;
+								case "resize": grid.igGridResizing("resize", state.value[0], state.value[1]); break;
+								case "group":
+									grid.igGridGroupBy("ungroupAll");
+									if (!grid.igGridGroupBy("checkColumnIsGrouped")) grid.igGridGroupBy("groupByColumn", state.value);
+									break;
+								case "hide":
+									if (state.value.split("_")[1]) {
+										grid.igGridHiding("hideColumn", state[0]);
+									} else {
+										grid.igGridHiding("showColumn", state[0]);
+									}
+									break;
+								case "filter": grid.igGridFiltering("filter", ([{ fieldName: state.value[0], expr: state.value[2], cond: state.value[1] }])); break;
+							}
+						} else { // Unload previous state
+							debugger;
+							prevState = window.History.savedStates[window.History.savedStates.length - 2].data;
+							if(prevState.key )
+							switch (prevState.key) {
+								case "page": grid.igGridPaging("pageIndex", 0);
+								case "filter": grid.igGridFiltering("filter", ([])); break;
+								case "sort": grid.igGridSorting("unsortColumn", prevState.value[0]); break;
+								default: break;
+							}
 						}
 					}
 					manualStateChange = true;
 
 					if (window.location.search === "" & tempParams !== "") {
-                		window.History.pushState(null, null, tempParams);
-                		tempParams = "";
+						window.History.pushState(null, null, tempParams);
+						tempParams = "";
 					}
 				});
-            }
+			}
 
-            function getPreviousStateOccurances(id) {
+            function getStateOccurances(id) {
             	var historyStates = window.History.savedStates, index, count = 0;
             	for (index = 0; index < historyStates.length - 1; index++) {
             		if (historyStates[index].id === id) count++;
             	}
             	return count;
+            }
+
+            function getLastOccurance(state) {
+            	var historyStates = window.History.savedStates, index;
             }
         	//<-- Recover igGrid state from the browser history object
 
@@ -274,11 +320,11 @@ $(function () {
             	state = state.substring(0, columnEndIndex);
             	return state;
             }
+        	//<-- Create URL
 
             function isEmptyValue(value) {
-                return value.length === 0;
+            	return value === undefined || value === null || value.length === 0;
             }
-        	//<-- Create URL
 
             $("#back").igButton().click(function () { window.History.back(); });
             $("#forward").igButton().click(function () { window.History.forward(); });
