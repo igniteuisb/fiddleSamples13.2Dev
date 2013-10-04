70bade9787a1e393dd5a5f02692b707dac87257e
Ignite UI Samples Browser deployer <igniteui.sb@gmail.com>
2013-10-04 12:54:18 -0400

    SB build 13.2.20132.1392

--- JA/HtmlSamples/grid/column-fixing/demo.js
+++ JA/HtmlSamples/grid/column-fixing/demo.js
@@ -1,22 +1,22 @@
 $(function () {
             $("#grid").igGrid({
                 columns: [
-                    { headerText: "顧客 ID", key: "ID", dataType: "string", width: "10%" },
-                    { headerText: "会社名", key: "CompanyName", dataType: "string", width: "15%" },
-                    { headerText: "名前", key: "ContactName", dataType: "string", width: "15%" },
-                    { headerText: "連絡先", key: "ContactTitle", dataType: "string", width: "15%" },
-                    { headerText: "住所", key: "Address", dataType: "string", width: "15%" },
-                    { headerText: "市", key: "City", dataType: "string", width: "15%" },
-                    { headerText: "国名", key: "Country", dataType: "string", width: "15%" }
+                    { headerText: "顧客 ID", key: "ID", dataType: "string", width: "150px" },
+                    { headerText: "会社名", key: "CompanyName", dataType: "string", width: "200px" },
+                    { headerText: "名前", key: "ContactName", dataType: "string", width: "200px" },
+                    { headerText: "連絡先", key: "ContactTitle", dataType: "string", width: "200px" },
+                    { headerText: "住所", key: "Address", dataType: "string", width: "200px" },
+                    { headerText: "市", key: "City", dataType: "string", width: "150px" },
+                    { headerText: "国名", key: "Country", dataType: "string", width: "150px" }
                 ],
                 autoGenerateColumns: false,
                 dataSource: nwCustomersWithOrders,
-                width: '100%',
+                width: "800px",
                 height: "400px",
                 features: [
                     {
                         name: "ColumnFixing",
-                        fixingDirection: 'left',
+                        fixingDirection: "left",
                         columnSettings: [
                             {
                                 columnKey: "CompanyName",
