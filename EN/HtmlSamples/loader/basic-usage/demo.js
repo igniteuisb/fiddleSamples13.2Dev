5440f65c3f3063c3f032b1a4132164188ab9b90b
Ignite UI Samples Browser deployer <igniteui.sb@gmail.com>
2013-10-31 09:06:21 -0400

    SB build 13.2.20132.1718

--- EN/HtmlSamples/loader/basic-usage/demo.js
+++ EN/HtmlSamples/loader/basic-usage/demo.js
@@ -1,7 +1,7 @@
 $(function () {
 $.ig.loader({
-	        scriptPath: "http://dev.igniteui.local/13-2/igniteui/js/",
-	        cssPath: "http://dev.igniteui.local/13-2/igniteui/css/",
+	        scriptPath: "http://dev.igniteui.local/13-2/IgniteUI/js/",
+	        cssPath: "http://dev.igniteui.local/13-2/IgniteUI/css/",
 	        resources: "igGrid.Sorting"
 	    });
 
