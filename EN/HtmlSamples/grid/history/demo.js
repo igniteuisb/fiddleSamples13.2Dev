36941d55aae2269e300ef583ecd98709de1738a9
Ignite UI Samples Browser deployer <igniteui.sb@gmail.com>
2013-11-14 09:24:52 -0500

    SB build 13.2.20132.1847

--- EN/HtmlSamples/grid/history/demo.js
+++ EN/HtmlSamples/grid/history/demo.js
@@ -332,8 +332,8 @@ $(function () {
             function isEmptyValue(value) {
                 return value === undefined || value === null || value.length === 0;
             }
-            $("#back").igButton().click(function () { window.History.back(); });
-            $("#forward").igButton().click(function () { window.History.forward(); });
+            $("#back").igButton().click(function () { window.history.back(); });
+            $("#forward").igButton().click(function () { window.history.forward(); });
             $("#copy").igButton().click(function () { window.prompt("Copy URL and open it in a new tab or browser", window.location); });
             $("#mail").igButton().click(function () {
                 var link = "mailto: "
